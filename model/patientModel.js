928bf9ea7bf4e78e9fac7681e74b0bdbc37d5ac3
Maziar <mhassanz@my.centennialcollege.ca>
2024-12-04 19:47:38 -0500

    first commit

---
+++ model/patientModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const PatientSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    bdate: { type: Date, required: true },
+    gender: { type: String, required: true },    
+    address: { type: String, required: true },
+    status: { type: String, required: true },
+    clinical: [{
+        bph: { type: Number, required: true, default: 0 },  // Blood Pressure High (systolic)
+        bpl: { type: Number, required: true, default: 0 },  // Blood Pressure Low (diastolic)
+        rr: { type: Number, required: true, default: 0 },   // Respiratory Rate
+        bol: { type: Number, required: true, default: 0 },  // Blood Oxygen Level
+        hbr: { type: Number, required: true, default: 0 }   // Heart Beat Rate
+    }]
+}, { timestamps: true });
+ 
+export default mongoose.model("patients", PatientSchema);
